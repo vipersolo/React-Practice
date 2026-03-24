276a4d91ffc0412f4660efb27e368ebf020a5b46
Antony Jose <antonyjose777aj@gmail.com>
2026-03-24 15:27:05 +0530

    useEffect unmount worked in Site.js , strictmode causes double unmount when revisiting

--- src/navigationpage/Home.js
+++ src/navigationpage/Home.js
@@ -1,15 +1,27 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom' // not Navigate . needed is useNavigate
+import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom"; // not Navigate . needed is useNavigate
 
+function Home() {
+  const navigate = useNavigate();
 
- function Home() {
-  const navigate = useNavigate()
+  useEffect(() => { // arrow function +  dependency []
+    //insdie arrow funciton ( function,return)
+    const timer = setInterval(() => { // takes funciton/reference + timer in 1000 = 1 second
+      console.log("tick");
+    }, 1000);
+
+    return () => { // this return is stored by react after first run due to [] empty dependency
+      clearInterval(timer);
+      console.log("timer reset or component unmounted.");
+    };
+  }, []);
+
   return (
     <div>
       <h2>Welcome Home</h2>
-      <button onClick={()=>navigate('/contact')}>Go to Contact</button> 
+      <button onClick={() => navigate("/contact")}>Go to Contact</button>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
